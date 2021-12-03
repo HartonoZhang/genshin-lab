ec9056c3fde4fc827c1d2d23603198ebdda648d3
HartonoZhang <hartonobestzx@gmail.com>
2021-12-03 14:27:33 +0700

    adding imgsrc for weapon

--- src/components/weapon-item/weapon-item.component.jsx
+++ src/components/weapon-item/weapon-item.component.jsx
@@ -4,16 +4,22 @@ const WeaponItem = ({ weapon, styleLength }) => {
   const { name, type, rarity, bonus_effect, skill, imgSrc } = weapon;
   return (
     <div className={`table${styleLength % 2} weapon-item`}>
-      <div className='weapon-name-container'>
+      <div className="weapon-name-container">
         <img
           className="weapon-img"
           src={`${process.env.PUBLIC_URL + imgSrc}`}
           alt="weapon"
         />
-        <span className='weapon-name'>{name}</span>
+        <span className="weapon-name">{name}</span>
       </div>
       <div>{type}</div>
-      <div>{rarity}</div>
+      <div className='weapon-rarity-container'>
+        <img
+          className="star-img"
+          src={`${process.env.PUBLIC_URL + rarity}`}
+          alt="rarity"
+        />
+      </div>
       <div>{bonus_effect}</div>
       <div>{skill}</div>
     </div>
