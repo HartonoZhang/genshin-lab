e1f962f768b0ad6dcb59d148b3d4334eeac8bd8f
HartonoZhang <hartonobestzx@gmail.com>
2021-12-04 16:00:31 +0700

    changing type sword to type weapon utils handler

--- src/redux/weapon/weapon.utils.js
+++ src/redux/weapon/weapon.utils.js
@@ -1,3 +1,3 @@
 export const filterTypeWeaponUtils = (listWeapon, typeWeapon) => {
-  return Object.keys(listWeapon).map((key) => listWeapon[key]).filter(key => key.type_sword === typeWeapon);
+  return Object.keys(listWeapon).map((key) => listWeapon[key]).filter(key => key.type_weapon === typeWeapon);
 };
